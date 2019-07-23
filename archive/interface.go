d105ba4a2e59991a3de06d5fd99df6eb5b08bdf8
yj.ge <yj.ge@tianrang-inc.com>
2019-07-23 14:14:27 +0800

    interface

--- archive/interface.go
+++ archive/interface.go
@@ -22,9 +22,20 @@ func (plane *Plane) Move() {
 	fmt.Printf("Plane moves at speed %dkm/h\n", plane.Speed)
 }
 
+func parse(v interface{}) {
+	switch v.(type) {
+	case Car:
+		fmt.Println("v is a car")
+	case Plane:
+		fmt.Println("v is a plane")
+	}
+}
+
 func main() {
 	car := Car{Speed: 100}
 	plane := Plane{Speed: 600}
 	car.Move()
 	plane.Move()
+	parse(car)
+	parse(plane)
 }
